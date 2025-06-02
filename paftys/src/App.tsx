2ceee1eb6ae159c74942c2343b28dd4f19b7676c
MasNicolas <mas.nicolas188@gmail.com>
2025-06-02 17:26:04 +0200

    NavBar-NOTFIXED
    Début de navbar, bug d'affichage

--- paftys/src/App.tsx
+++ paftys/src/App.tsx
@@ -3,16 +3,20 @@ import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
 import { SignupForm } from "./components/ui/signup-form";
 import { ThemeProvider } from "./components/theme-provider";
+import { Sidebar } from "lucide-react";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Router>
+        <Sidebar>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<AuthPage />} />
           <Route path="/signup" element={<AuthPage form={<SignupForm />} />} />
         </Routes>
+        </Sidebar>
       </Router>
     </ThemeProvider>
   );
