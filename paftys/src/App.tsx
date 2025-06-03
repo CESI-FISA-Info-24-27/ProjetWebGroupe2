a4bb0d37c4eb5ca889ec2fe410a0301067fdc180
MasNicolas <mas.nicolas188@gmail.com>
2025-06-03 12:25:40 +0200

    sidebar update

--- paftys/src/App.tsx
+++ paftys/src/App.tsx
@@ -3,45 +3,16 @@ import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
 import { SignupForm } from "./components/ui/signup-form";
 import { ThemeProvider } from "./components/theme-provider";
-import {
-  SidebarProvider,
-  Sidebar,
-  SidebarContent,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-  SidebarInset,
-  SidebarTrigger,
-} from "./components/ui/sidebar";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Router>
-        <SidebarProvider>
-          <div className="flex">
-            <Sidebar>
-              <SidebarContent>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>Home</SidebarMenuButton>
-                  </SidebarMenuItem>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton>Login</SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-              </SidebarContent>
-            </Sidebar>
-            <SidebarInset>
-              <SidebarTrigger />
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<AuthPage />} />
-                <Route path="/signup" element={<AuthPage form={<SignupForm />} />} />
-              </Routes>
-            </SidebarInset>
-          </div>
-        </SidebarProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<AuthPage />} />
+          <Route path="/signup" element={<AuthPage form={<SignupForm />} />} />
+        </Routes>
       </Router>
     </ThemeProvider>
   );
