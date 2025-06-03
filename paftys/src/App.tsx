82244eb9a51b222644a3eaf7f85d7121d5a2ef6b
MasNicolas <mas.nicolas188@gmail.com>
2025-06-03 09:19:29 +0200

    NAVBAR-fixed
    NAVBAR is fixed

--- paftys/src/App.tsx
+++ paftys/src/App.tsx
@@ -3,23 +3,47 @@ import Home from "./pages/Home";
 import AuthPage from "./pages/AuthPage";
 import { SignupForm } from "./components/ui/signup-form";
 import { ThemeProvider } from "./components/theme-provider";
-import { Sidebar } from "lucide-react";
+import {
+  SidebarProvider,
+  Sidebar,
+  SidebarContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarInset,
+  SidebarTrigger,
+} from "./components/ui/sidebar";
 
 const App = () => {
   return (
     <ThemeProvider>
       <Router>
-        <Sidebar>
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<AuthPage />} />
-          <Route path="/signup" element={<AuthPage form={<SignupForm />} />} />
-        </Routes>
-        </Sidebar>
+        <SidebarProvider>
+          <div className="flex">
+            <Sidebar>
+              <SidebarContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton>Home</SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton>Login</SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarContent>
+            </Sidebar>
+            <SidebarInset>
+              <SidebarTrigger />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<AuthPage />} />
+                <Route path="/signup" element={<AuthPage form={<SignupForm />} />} />
+              </Routes>
+            </SidebarInset>
+          </div>
+        </SidebarProvider>
       </Router>
     </ThemeProvider>
   );
 };
-
 export default App;
