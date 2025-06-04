021d288d9f8f3e47c8dfe1cc5611742307af7806
Benjamin <145976418+BenRep@users.noreply.github.com>
2025-06-04 16:09:17 +0200

    Setup redux & get all posts

--- paftys/src/main.tsx
+++ paftys/src/main.tsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import { configureStore } from "@reduxjs/toolkit";
+import { getAllPosts } from "./actions/post.action.ts";
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
+store.dispatch(getAllPosts());
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
