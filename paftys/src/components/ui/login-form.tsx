33fd19410eaffa99b8b98a6e43650b717cf164fd
Benjamin <145976418+BenRep@users.noreply.github.com>
2025-06-10 11:36:24 +0200

    Refactor login and signup pages

--- paftys/src/components/ui/login-form.tsx
+++ paftys/src/components/ui/login-form.tsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { login } from "@/reducers/authSlice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import store from "@/redux/store";
 
 export function LoginForm({
@@ -35,9 +35,9 @@ export function LoginForm({
       onSubmit={handleSubmit}
     >
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">Se connecter à votre compte</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          Entrez votre adresse e-mail et votre mot de passe.
         </p>
       </div>
 
@@ -45,11 +45,11 @@ export function LoginForm({
 
       <div className="grid gap-6">
         <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
+          <Label htmlFor="email">E-mail</Label>
           <Input
             id="email"
             type="email"
-            placeholder="m@example.com"
+            placeholder="exemple@gmail.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
@@ -58,13 +58,7 @@ export function LoginForm({
 
         <div className="grid gap-3">
           <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
+            <Label htmlFor="password">Mot de passe</Label>
           </div>
           <Input
             id="password"
@@ -75,59 +69,16 @@ export function LoginForm({
           />
         </div>
 
-        <Button type="submit" className="w-full" disabled={loading}>
-          {loading ? "Connexion..." : "Login"}
-        </Button>
-
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Or continue with
-          </span>
-        </div>
-
-        <Button variant="outline" className="w-full">
-          {/* Icon GitHub */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            className="w-4 h-4 mr-2"
-          >
-            <path
-              d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 
-              3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 
-              0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61 
-              C4.422 18.07 3.633 17.7 3.633 17.7 
-              c-1.087-.744.084-.729.084-.729 
-              1.205.084 1.838 1.236 1.838 1.236 
-              1.07 1.835 2.809 1.305 3.495.998 
-              .108-.776.417-1.305.76-1.605 
-              -2.665-.3-5.466-1.332-5.466-5.93 
-              0-1.31.465-2.38 1.235-3.22 
-              -.135-.303-.54-1.523.105-3.176 
-              0 0 1.005-.322 3.3 1.23 
-              .96-.267 1.98-.399 3-.405 
-              1.02.006 2.04.138 3 .405 
-              2.28-1.552 3.285-1.23 3.285-1.23 
-              .645 1.653.24 2.873.12 3.176 
-              .765.84 1.23 1.91 1.23 3.22 
-              0 4.61-2.805 5.625-5.475 5.92 
-              .42.36.81 1.096.81 2.22 
-              0 1.606-.015 2.896-.015 3.286 
-              0 .315.21.69.825.57 
-              C20.565 22.092 24 17.592 24 12.297 
-              c0-6.627-5.373-12-12-12"
-              fill="currentColor"
-            />
-          </svg>
-          Login with GitHub
+        <Button type="submit" className="w-full cursor-pointer" disabled={loading}>
+          {loading ? "Connexion..." : "Se connecter"}
         </Button>
       </div>
 
       <div className="text-center text-sm">
-        Don&apos;t have an account?{" "}
-        <a href="/signup" className="underline underline-offset-4">
-          Sign up
-        </a>
+        Vous n'avez pas encore de compte ?{" "}
+        <Link to="/signup" className="underline underline-offset-4">
+          S'enregistrer
+        </Link>
       </div>
     </form>
   );
--- paftys/src/pages/AuthPage.tsx
+++ paftys/src/pages/AuthPage.tsx
@@ -1,30 +1,32 @@
+import fullLogo from "@/assets/full_logo_paftys.svg";
 import logo from "@/assets/p_logo_paftys.svg";
 import { LoginForm } from "@/components/ui/login-form";
+import { Link } from "react-router-dom";
 
 export default function AuthPage({ form }: { form?: React.ReactNode }) {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh w-full lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="/" className="flex items-center gap-2 font-medium">
+          <Link to="/" className="flex items-center gap-2 font-medium">
             <img
               src={logo}
               alt="Logo"
               className="w-10 h-10 rounded-full object-cover"
             />
             Paftys
-          </a>
+          </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">{form || <LoginForm />}</div>
         </div>
       </div>
       <div className="bg-muted relative hidden lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+        <img  
+          src={fullLogo}
+          alt="logo"
+          className="h-full w-full scale-75"
+          />
       </div>
     </div>
   );
