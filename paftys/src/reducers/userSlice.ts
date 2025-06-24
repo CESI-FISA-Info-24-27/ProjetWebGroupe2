d02f52f5e8411aedc16230bf3145233a525d8162
Xavier Ferber <xavier.ferber@viacesi.fr>
2025-06-24 17:19:26 +0200

    wip subscriptions

--- paftys/src/reducers/userSlice.ts
+++ paftys/src/reducers/userSlice.ts
@@ -51,6 +51,32 @@ export const fetchUserById = createAsyncThunk<User, { userName: string }>(
   }
 );
 
+interface ToggleSubscriptionResponse {
+  myInfo: User;
+  subscribedUserInfo: User;
+}
+
+export const toggleSubscription = createAsyncThunk<
+  ToggleSubscriptionResponse, // payload type when fulfilled
+  { userId: string }, // argument type
+  { rejectValue: string } // payload type when rejected
+>("user/toggleSubscription", async ({ userId }, thunkAPI) => {
+  try {
+    const token = Cookies.get("token");
+    const res = await axios.post(
+      `${API_BASE_URL}/subscribe/`,
+      { userIdToSubscribe: userId },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    return res.data.data;
+  } catch (err: any) {
+    return thunkAPI.rejectWithValue(
+      err.response?.data?.message || "Erreur lors du toggle abonnement."
+    );
+  }
+});
 // Slice Redux Toolkit
 const userSlice = createSlice({
   name: "user",
@@ -81,6 +107,47 @@ const userSlice = createSlice({
       .addCase(fetchUserById.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload as string;
+      })
+      .addCase(toggleSubscription.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(toggleSubscription.fulfilled, (state, action) => {
+        state.loading = false;
+
+        const payload = action.payload;
+        if (!payload) return;
+
+        console.log("Toggle subscription payload:", payload);
+        const { myInfo, subscribedUserInfo } = payload;
+
+        const myIndex = state.users.findIndex((u) => u.id === myInfo.id);
+        if (myIndex !== -1) {
+          state.users[myIndex] = {
+            ...state.users[myIndex],
+            ...myInfo,
+          };
+        } else {
+          state.users.push(myInfo);
+        }
+
+        const subscribedIndex = state.users.findIndex(
+          (u) => u.id === subscribedUserInfo.id
+        );
+        if (subscribedIndex !== -1) {
+          state.users[subscribedIndex] = {
+            ...state.users[subscribedIndex],
+            ...subscribedUserInfo,
+          };
+        } else {
+          state.users.push(subscribedUserInfo);
+        }
+      })
+
+      .addCase(toggleSubscription.rejected, (state, action) => {
+        state.loading = false;
+        state.error =
+          action.error.message || "Erreur lors du toggle abonnement.";
       });
   },
 });
