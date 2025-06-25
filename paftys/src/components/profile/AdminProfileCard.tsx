0a9c8eb69a05a894075b0af95ba1ea6283acb4e6
Benjamin <145976418+BenRep@users.noreply.github.com>
2025-06-25 17:01:39 +0200

    Better UI

--- paftys/src/components/profile/AdminProfileCard.tsx
+++ paftys/src/components/profile/AdminProfileCard.tsx
@@ -19,12 +19,32 @@ export default function AdminProfileCard({user} : {user:any}) {
   return (
     <Card className="flex flex-row items-center justify-center transition-all duration-300 ease-in-out lg:hover:scale-102 m-4 p-2">
       <Link to={"/profile/" + user?.userName} className="w-[70%] lg:text-xl md:text-lg hover:text-purple-600">{user?.userName}</Link>
-      <Button onClick={() => toggleSuspend(user?._id)} title="Suspendre l'utilisateur" className="w-10 h-10 cursor-pointer">
-        <i className="bi bi-pause-circle"></i>
-      </Button>
-      <Button onClick={() => toggleBan(user?._id )} title="Bannir l'utilisateur" className="w-10 h-10 cursor-pointer">
-        <i className="bi bi-x-octagon"></i>
-      </Button>
+      {user.state === "normal" && (
+        <>
+        <Button onClick={() => toggleSuspend(user?._id)} title="Suspendre l'utilisateur" className="w-10 h-10 bg-gray-600 hover:bg-gray-500 cursor-pointer">
+          <i className="bi bi-pause-circle"></i>
+        </Button>
+        <Button onClick={() => toggleBan(user?._id )} title="Bannir l'utilisateur" className="w-10 h-10 bg-red-700 hover:bg-red-600 cursor-pointer">
+          <i className="bi bi-x-octagon"></i>
+        </Button>
+        </>
+      )}
+      {user.state === "banned" && (
+        <>
+        <div className="w-10 h-10"/>
+        <Button onClick={() => toggleBan(user?._id )} title="Débannir l'utilisateur" className="w-10 h-10 bg-green-700 hover:bg-green-600 cursor-pointer">
+          <i className="bi bi-arrow-counterclockwise"></i>
+        </Button>
+        </>
+      )}
+      {user.state === "suspended" && (
+        <>
+        <Button onClick={() => toggleSuspend(user?._id)} title="Rétablir l'utilisateur" className="w-10 h-10 bg-green-700 hover:bg-green-600 cursor-pointer">
+          <i className="bi bi-play-circle"></i>
+        </Button>
+        <div className="w-10 h-10"/>
+        </>
+      )}
     </Card>
   )
 }
